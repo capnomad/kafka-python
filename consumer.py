0941009eecc7f05e3d4e3a25463898a7b1563839
Mohammad Jamal Khan <khanmohammadjamal@gmail.com>
2022-06-23 14:19:34 +0530

    added producer-consumer

---
+++ consumer.py
@@ -0,0 +1,31 @@
+# Import KafkaConsumer from Kafka library
+from kafka import KafkaConsumer
+import sys
+import configparser 
+
+# Reading config from kafka_conf.ini
+config = configparser.ConfigParser()
+config.read('kafka_conf.ini')
+
+# Define server with port
+bootstrap_servers =config['kafka_config']['bootstrap_server']
+
+# Define topic name from where the message will recieve
+topicName = config['kafka_config']['topic']
+
+# Initialize consumer variable
+consumer = KafkaConsumer (topicName, group_id = 'group1',bootstrap_servers = bootstrap_servers,
+auto_offset_reset = 'earliest')
+print("~~~~~~~~~~~ Waiting for producer to publish ~~~~~~~~~~~")
+
+# Read and print message from consumer
+try:
+    for msg in consumer:
+        print("###########Message received##############")
+        print(msg.topic, msg.value)
+
+        print("\n~~~~~~~~~~~ Waiting for producer to publish ~~~~~~~~~~~")
+except KeyboardInterrupt:
+    print("Stopping consumer !!!")
+    # Terminate the script
+    sys.exit()
---
+++ producer.py
@@ -0,0 +1,34 @@
+# Import KafkaProducer from Kafka library
+from kafka import KafkaProducer
+import configparser 
+import sys
+
+# Reading config from kafka_conf.ini
+config = configparser.ConfigParser()
+config.read('kafka_conf.ini')
+
+# Define server with port
+bootstrap_servers = config['kafka_config']['bootstrap_server']
+
+# Define topic name where the message will publish
+topicName = config['kafka_config']['topic']
+
+# Initialize producer variable
+producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
+
+# Publish text in defined topic
+while True:
+    try:
+        print("\nEnter message to send: ", end="")
+        msg = input()
+        ack = producer.send(topicName, bytes(msg, 'utf8'))
+
+        # Print message
+        print("######Message Sent#########")
+        metadata = ack.get()
+        print("Topic: ", metadata.topic)
+        print("Partiton: ", metadata.partition)
+        print("Message: ", msg)
+    except KeyboardInterrupt:
+        print("Shutting down Producer !!!!")
+        sys.exit()
